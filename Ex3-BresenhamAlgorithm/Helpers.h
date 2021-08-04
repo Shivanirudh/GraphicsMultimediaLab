2735ad860292b30eb2be7cbd29d3e85c9e5c4ddc
Shivanirudh <shivanirudh2001@gmail.com>
2021-08-04 11:45:29 +0530

    Ex 3 Bresenham's Line Drawing Algorithm

---
+++ Ex3-BresenhamAlgorithm/Helpers.h
@@ -0,0 +1,118 @@
+#include "Signatures.h"
+
+bool initGL(){
+    //Initialize Projection Matrix
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+	gluOrtho2D(0.0,640.0,0.0,480.0);
+	
+    //Initialize Modelview Matrix
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+
+    glTranslatef( SCREEN_WIDTH / 3.f, SCREEN_HEIGHT / 3.f, 0.f );
+
+    //Initialize clear color
+    glClearColor( 0.f, 0.f, 0.f, 1.f );
+
+    glPointSize(POINT_SIZE);
+    glEnable(GL_POINT_SMOOTH);
+
+    //Check for error
+    GLenum error = glGetError();
+    if( error != GL_NO_ERROR )
+    {
+        printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+        return false;
+    }
+
+    return true;
+}
+
+void update(){
+
+}
+
+void render(){
+    vector<pair<int, int>> points = Bresenham();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1,1,1);
+    glBegin(GL_POINTS);
+        for(pair<int, int> p: points){
+            glVertex2d(p.first, p.second);
+        }
+    glEnd();
+    glFlush(); 
+}
+
+void selectOctant(int option){
+    X0 = coords[option-1][0]*20;
+    Y0 = coords[option-1][1]*20;
+    Xn = coords[option-1][2]*20;
+    Yn = coords[option-1][3]*20;
+}
+
+int sign(int val){
+	if(val<0){
+		return -1;
+	}
+	else if(val>0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+vector<pair<int, int>> Bresenham(){
+	
+	int x=X0;
+	int y=Y0;
+
+	int dx=abs(Xn-X0);
+	int dy=abs(Yn-Y0);
+
+	int sign_x = sign(Xn-X0);
+	int sign_y = sign(Yn-Y0);
+
+	int swap=-1;
+
+	//Swap x and y if slope > 1, and acknowledge swap.
+	if(dy>dx){
+		int temp = dx;
+		dx = dy;
+		dy = temp;
+		swap=1;
+	}
+	else{
+		swap=0;
+	}
+
+	int p = (2*dy) - dx;
+	int faca = 2*dy;
+	int facb = (2*dy)-(2*dx);
+
+	vector<pair<int, int>> points;
+
+	points.push_back(pair<int, int>(x, y));
+
+	for(int i=1;i<=dx;i++){
+		if(p<0){
+			if(swap==1){
+				y += sign_y;
+			}
+			else{
+				x += sign_x; 
+			}
+			p = p + faca;
+		}
+		else{
+			y += sign_y;
+			x += sign_x;
+			p += facb;
+		}
+		points.push_back(pair<int, int>(x, y));
+	}
+	return points;
+}
---
+++ Ex3-BresenhamAlgorithm/main.cpp
@@ -0,0 +1,55 @@
+#include "Helpers.h"
+
+void runMainLoop(int val);
+
+int main( int argc, char* args[] ){
+
+	glutInit( &argc, args );
+
+	glutInitContextVersion( 2, 1 );
+
+	glutInitDisplayMode( GLUT_SINGLE|GLUT_RGB );
+	glutInitWindowSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+	glutCreateWindow( "OpenGL" );
+
+	int option=0;
+	cout<<"Choose octant: (1 to 8 both inclusive): ";
+	cin>>option;
+
+	selectOctant(option);
+	cout<<"Start point: ("<<X0<<", "<<Y0<<")"<<endl;
+	cout<<"End point: ("<<Xn<<", "<<Yn<<")"<<endl;
+
+	if( !initGL() )
+	{
+		printf( "Unable to initialize graphics library!\n" );
+		return 1;
+	}
+
+	vector<pair<int, int>> points = Bresenham();
+	int count=0;
+	cout<<"Points plotted: "<<endl;
+	for(pair<int, int> p: points){
+        cout<<"("<<p.first<<", "<<p.second<<")"<<" ";
+        count++;
+        if(count==4){
+        	count=0;
+        	cout<<endl;
+        }
+    }
+
+	glutDisplayFunc( render );
+
+	glutTimerFunc( 1000 / SCREEN_FPS, runMainLoop, 0 );
+
+	glutMainLoop();
+
+	return 0;
+}
+
+void runMainLoop( int val ){
+    update();
+    render();
+
+    glutTimerFunc( 1000 / SCREEN_FPS, runMainLoop, val );
+}
